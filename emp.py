a770e8fbfbeeb026320fc9ae3e5f9c9bb79354a9
basavarajandanshetti-glitch <basavarajandanshetti@gmail.com>
2025-11-20 15:31:32 +0530

    Update usage message and add salary and experience

--- emp.py
+++ emp.py
@@ -1,11 +1,12 @@
 import sys
 if len(sys.argv) != 3:
-    print("Usage: script.py <name> <rollno>")
+    print("Usage: script.py <name> <rollno> <salary> <exp>")
     sys.exit(1)
     script_name = sys.argv[0]
     name = sys.argv[1]
     id = sys.argv[2]
     salary = sys.argv[3]
+    exp = sys.argv[4]
 print(f"Script Name: {script_name}")
 print(f"Name: {name}")
 print(f"Roll Number: {rollno}")
