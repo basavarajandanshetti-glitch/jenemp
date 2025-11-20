59a546d86ca6046b77202ab76346ad0abcc6e3b5
basavarajandanshetti-glitch <basavarajandanshetti@gmail.com>
2025-11-20 15:44:06 +0530

    Update emp.py

--- emp.py
+++ emp.py
@@ -1,13 +1,19 @@
 import sys
-if len(sys.argv) != 3:
+
+# Expecting 4 arguments: name, rollno, salary, exp
+if len(sys.argv) != 5:
     print("Usage: script.py <name> <rollno> <salary> <exp>")
     sys.exit(1)
-    script_name = sys.argv[0]
-    name = sys.argv[1]
-    id = sys.argv[2]
-    salary = sys.argv[3]
-    exp = sys.argv[4]
+
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+salary = sys.argv[3]
+exp = sys.argv[4]
+
 print(f"Script Name: {script_name}")
 print(f"Name: {name}")
 print(f"Roll Number: {rollno}")
-print(f"salary: {salary}")
+print(f"Salary: {salary}")
+print(f"Experience: {exp}")
+
